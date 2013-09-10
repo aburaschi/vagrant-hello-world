bd77a7264cedd90cb9ee6b0a22592ec5bd0e9fc0
Andres Buraschi <andres.buraschi@intel.com>
2013-09-10 08:29:21 -0300

    Adding basic Vagrant configuration and a simple Http site to show in the webbrowser. The bootstrap.sh file installs apache, vim and django (+sqlite3). Django is not used by any project yet.
    Accessible in: localhost:4567/MyHttpSite

---
+++ bootstrap.sh
@@ -0,0 +1,11 @@
+﻿#!/usr/bin/env bash
+
+export http_proxy=http://proxy-us.intel.com:911
+export https_proxy=http://proxy-us.intel.com:911
+apt-get update
+apt-get install -y apache2
+rm -rf /var/www
+ln -fs /vagrant /var/www
+apt-get install -y vim
+apt-get install -y python-django
+apt-get install -y sqlite3
